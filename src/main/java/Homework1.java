01a475d02da97674ce4e6cc0c3e7f1a3a48e6ca0
Kereot <eepdmitriy@gmail.com>
2022-02-16 04:19:27 +0300

    HW6 tasks 1 and 2

---
+++ src/main/java/Homework1.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Homework1 {
+
+    public int[] getTailNoList(int[] arr, int point) {
+        if (arr.length == 0) { // Т.к. по заданию передаётся "непустой" массив.
+            throw new IllegalArgumentException();
+        }
+
+        int a = 0;
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            if (arr[arr.length - 1] == point) {
+                return new int[]{};
+            }
+            if (arr[i] == point) {
+                a = i;
+                break;
+            }
+            if (i == 0 && arr[i] != point) {
+                throw new RuntimeException();
+            }
+        }
+
+        return Arrays.copyOfRange(arr, a + 1, arr.length);
+    }
+
+    public int[] getTail(int[] arr, int point) { // Изначально это был мой первый вариант, но приведение к int пришлось погуглить.
+        if (arr.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            if (arr[i] == point) {
+                break;
+            } else {
+                list.addFirst(arr[i]);
+            }
+            if (i == 0 && arr[i] != point) {
+                throw new RuntimeException();
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] getTailAlt (int[] arr, int point) { // Захотелось попробовать с .lastIndexOf без fori, но конвертация int <-> Integer стандартными средствами здесь показала себя ещё более страшной.
+        if (arr.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        ArrayList<Integer> list = (ArrayList<Integer>) Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        int a = list.lastIndexOf(point);
+        if (a == -1) {
+            throw new RuntimeException();
+        }
+        int[] tail = list.subList(a + 1, list.size()).stream().mapToInt(Integer::intValue).toArray();
+
+        return tail;
+    }
+}
---
+++ src/test/java/TestHW1.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestHW1 {
+    private Homework1 hw1;
+
+    @BeforeEach
+    public void init() {
+        hw1 = new Homework1();
+    }
+
+    public static Stream<Arguments> arrayCheck() {
+        List<Arguments> out = new ArrayList<>();
+        out.add(Arguments.arguments(new int[] {1, 2, 4, 4, 2, 3, 4, 1, 7}, 4, new int[] {1, 7}));
+        out.add(Arguments.arguments(new int[] {1, 2, 4, 1, 7, 5, 3, 2}, 4, new int[] {1, 7, 5, 3, 2}));
+        out.add(Arguments.arguments(new int[] {1 , 2, 4}, 4, new int[]{}));
+        return out.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayCheck")
+    public void testGetTail(int[] a, int b, int[] result) {
+        Assertions.assertArrayEquals(result, hw1.getTail(a, b));
+    }
+    @ParameterizedTest
+    @MethodSource("arrayCheck")
+    public void testGetTailAlt(int[] a, int b, int[] result) {
+        Assertions.assertArrayEquals(result, hw1.getTailAlt(a, b));
+    }
+    @ParameterizedTest
+    @MethodSource("arrayCheck")
+    public void testGetTailNoList(int[] a, int b, int[] result) {
+        Assertions.assertArrayEquals(result, hw1.getTailNoList(a, b));
+    }
+
+    @Test
+    public void rExGetTail() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+           hw1.getTail(new int[] {1, 2, 5, 6}, 4);
+        });
+    }
+    @Test
+    public void rExGetTailAlt() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            hw1.getTailAlt(new int[] {1, 2, 5, 6}, 4);
+        });
+    }
+    @Test
+    public void rExGetTailNoList() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            hw1.getTailNoList(new int[] {1, 2, 5, 6}, 4);
+        });
+    }
+    @Test
+    public void iExGetTail() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            hw1.getTail(new int[] {}, 4);
+        });
+    }
+    @Test
+    public void iExGetTailAlt() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            hw1.getTailAlt(new int[] {}, 4);
+        });
+    }
+    @Test
+    public void iExGetTailNoList() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            hw1.getTailNoList(new int[] {}, 4);
+        });
+    }
+}
